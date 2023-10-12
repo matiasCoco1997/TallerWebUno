8562b1dc54a071b805607c709023c097dd1ec924
Matias Ariel Coco <cocomatias6@gmail.com>
2023-10-12 18:42:45 -0300

    update para presentacion 12-10-2023

--- src/main/java/com/tallerwebi/dominio/servicios/ServicioNoticia.java
+++ src/main/java/com/tallerwebi/dominio/servicios/ServicioNoticia.java
@@ -3,17 +3,14 @@ package com.tallerwebi.dominio.servicios;
 import com.tallerwebi.dominio.entidades.Categoria;
 import com.tallerwebi.dominio.entidades.Noticia;
 import com.tallerwebi.dominio.entidades.Usuario;
-import com.tallerwebi.dominio.excepcion.CampoVacio;
-import com.tallerwebi.dominio.excepcion.CategoriaInexistente;
-import com.tallerwebi.dominio.excepcion.FormatoDeImagenIncorrecto;
-import com.tallerwebi.dominio.excepcion.TamanioDeArchivoSuperiorALoPermitido;
+import com.tallerwebi.dominio.excepcion.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface ServicioNoticia {
-    void crearNoticia(Noticia noticia, Usuario usuarioLogueado, MultipartFile imagen, MultipartFile audio) throws CampoVacio, TamanioDeArchivoSuperiorALoPermitido, IOException, FormatoDeImagenIncorrecto;
+    void crearNoticia(Noticia noticia, Usuario usuarioLogueado, MultipartFile imagen, MultipartFile audio) throws CampoVacio, TamanioDeArchivoSuperiorALoPermitido, IOException, FormatoDeImagenIncorrecto, FormatoDeAudioIncorrecto;
 
     void borrarNoticiaPorId(Long idNoticia);
 
