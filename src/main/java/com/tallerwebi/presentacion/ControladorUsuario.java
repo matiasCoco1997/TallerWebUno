b5bd84d6511dd3cb76222cbb7729f3df9ed5b276
Matias Ariel Coco <cocomatias6@gmail.com>
2023-10-15 09:03:36 -0300

    update

--- src/main/java/com/tallerwebi/presentacion/ControladorUsuario.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorUsuario.java
@@ -23,23 +23,46 @@ public class ControladorUsuario {
 
     @Autowired
     public ControladorUsuario(ServicioUsuario servicioUsuario){
-        this.servicioUsuario=servicioUsuario;
+        this.servicioUsuario = servicioUsuario;
     }
 
     @RequestMapping(value = "/perfil",method = {RequestMethod.GET,RequestMethod.POST})
     public ModelAndView perfil(@RequestParam(value = "idUsuario",required = false)Long id, HttpSession session){
         ModelMap model=new ModelMap();
-        List<Categoria> categorias=servicioUsuario.obtenerCategorias();
+        List<Categoria> categorias = servicioUsuario.obtenerCategorias();
         model.put("categorias",categorias);
         try{
 
-            Usuario usuario= (servicioUsuario.verificarSiElIDEsNull(id)) ?
-                                    (Usuario) session.getAttribute("sessionUsuarioLogueado") :
-                                            servicioUsuario.obtenerUsuarioPorId(id);
-            Usuario usuario2=(Usuario) session.getAttribute("sessionUsuarioLogueado");
-            List<Noticia> noticiasDelUsuario= servicioUsuario.obtenerNoticiasDeUnUsuario(usuario.getIdUsuario());
+            Usuario usuario = (servicioUsuario.verificarSiElIDEsNull(id)) ? (Usuario) session.getAttribute("sessionUsuarioLogueado") : servicioUsuario.obtenerUsuarioPorId(id);
+            Usuario usuarioLogueado = (Usuario) session.getAttribute("sessionUsuarioLogueado");
+            List<Noticia> noticiasDelUsuario = servicioUsuario.obtenerNoticiasDeUnUsuario(usuario.getIdUsuario());
             model.put("usuario",usuario);
-            model.put("usuario2",usuario2);
+            model.put("usuarioLogueado",usuarioLogueado);
+            model.put("noticias",noticiasDelUsuario);
+
+            if(servicioUsuario.verificarSiLaDescripcionEsNull(usuario.getDescripcion())){
+                model.put("descripcionError", "No tiene una descripción.");
+            }
+
+        }catch (Exception e){
+            model.put("errorUsuario","No existe un usuario con ese ID");
+            return new ModelAndView("usuarioNoEncontrado",model);
+        }
+        return new ModelAndView("perfil",model);
+    }
+
+    @RequestMapping(value = "/perfil/borradores",method = RequestMethod.GET)
+    public ModelAndView verNoticiasEnEstadoBorrador(@RequestParam(value = "idUsuario",required = false)Long id, HttpSession session){
+        ModelMap model = new ModelMap();
+        List<Categoria> categorias = servicioUsuario.obtenerCategorias();
+        model.put("categorias",categorias);
+        try{
+
+            Usuario usuario = (servicioUsuario.verificarSiElIDEsNull(id)) ? (Usuario) session.getAttribute("sessionUsuarioLogueado") : servicioUsuario.obtenerUsuarioPorId(id);
+            Usuario usuarioLogueado =(Usuario) session.getAttribute("sessionUsuarioLogueado");
+            List<Noticia> noticiasDelUsuario = servicioUsuario.obtenerNoticiasDeUnUsuario(usuario.getIdUsuario());
+            model.put("usuario",usuario);
+            model.put("usuarioLogueado",usuarioLogueado);
             model.put("noticias",noticiasDelUsuario);
 
             if(servicioUsuario.verificarSiLaDescripcionEsNull(usuario.getDescripcion())){
