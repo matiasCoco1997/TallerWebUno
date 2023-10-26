26964c91089464e542c5e928a4555228560bc0eb
Matias Ariel Coco <cocomatias6@gmail.com>
2023-10-26 00:27:23 -0300

    update - falta ver cuando esta con el like en blanco

--- src/main/webapp/resources/core/js/likearNoticia.js
+++ src/main/webapp/resources/core/js/likearNoticia.js
@@ -1,35 +1,33 @@
 $(document).ready(function () {
     $(document).on("click", ".dar-like-btn", function (event) {
 
-        var buttonId = $(this).attr('id'); // Obtiene el ID del botón que se ha hecho clic
+        var buttonId = $(this).attr('id');
 
         $.ajax({
-            type: "POST", // O el método que necesites
-            url: "/sunn/noticia/likear", // La URL a la que deseas enviar la solicitud
+            type: "POST",
+            url: "/sunn/noticia/likear",
             data: {
                 idNoticia: buttonId
             },
             success: function(response) {
 
-                var cantidadMG;
-                var colorDelBoton = $('.like_' + buttonId).css('fill');
-                console.log('Color actual:', colorDelBoton);
+                var cantidadMG = parseInt($('.cantidadMG_'+ buttonId).text(), 10);
 
-                if(colorDelBoton === "rgb(0, 128, 0)"){
-                    $('.like_'+ buttonId).css('fill', 'white');
-                } else {
-                    $('.like_'+ buttonId).css('fill', 'green');
+                if(isNaN(cantidadMG)){
+                    cantidadMG = 0;
+                    $('.cantidadMG_'+ buttonId).text(cantidadMG);
                 }
-
+                console.log(response);
                 if(response){
-                    //aca deberia sumar un me gusta a lo que tengo
-                    cantidadMG = $('.cantidadMG_'+ buttonId).text();
-                    console.log(cantidadMG);
                     cantidadMG++;
-                    console.log(cantidadMG);
+                    $('.cantidadMG_'+ buttonId).text(cantidadMG);
+                    $('.like_'+ buttonId).css('fill', 'green');
                 } else {
-                    //deberia restar
+                    cantidadMG--;
+                    $('.cantidadMG_'+ buttonId).text(cantidadMG);
+                    $('.like_'+ buttonId).css('fill', 'white');
                 }
+                console.log("cantidad de mg = " + cantidadMG);
             },
             error: function(error) {
                 console.error(error);
