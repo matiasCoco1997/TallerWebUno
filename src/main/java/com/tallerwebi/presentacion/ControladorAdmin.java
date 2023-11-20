68947cb832f4e602391954ae4fb95b9cc2d20ae6
Matias Ariel Coco <cocomatias6@gmail.com>
2023-11-20 12:38:35 -0300

    update - falta la funcion eliminar usuario

--- src/main/java/com/tallerwebi/presentacion/ControladorAdmin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorAdmin.java
@@ -13,6 +13,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
@@ -79,8 +80,8 @@ public class ControladorAdmin {
         return new ModelAndView("usuariosActivos",model);
     }
 
-    @GetMapping("/admin/eliminarUsuario/{idUsuario}")
-    public ModelAndView eliminarUsuario(HttpSession session , @PathVariable Long idUsuario){
+    @RequestMapping(value = "/eliminarUsuario/{idUsuario}", method = RequestMethod.GET)
+    public ModelAndView eliminarUsuario(HttpSession session , @PathVariable(required = false) Long idUsuario){
 
         ModelMap model=new ModelMap();
 
@@ -90,11 +91,13 @@ public class ControladorAdmin {
             return new ModelAndView("redirect:/login");
         }
 
-        List<Usuario> usuariosActivos = servicioHome.listarUsuarios(usuario.getIdUsuario());
+        try {
+            //servicioUsuario.borrarUsuario(idUsuario); borrar el usuario
+        } catch (Exception e) {
+            model.put("error", "Error al borrar usuario.");
+            return new ModelAndView("usuariosActivos", model);
+        }
 
-        model.put("usuario",usuario);
-        model.put("usuariosActivos", usuariosActivos);
-
-        return new ModelAndView("usuariosActivos",model);
+        return new ModelAndView("redirect:/admin/usuarios");
     }
 }
